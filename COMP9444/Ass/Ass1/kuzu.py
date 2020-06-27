9fd09ec362066f11025d79a3833e8e7b90d6f0e7
IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
2020-06-27 17:28:23 +1000

    Ass updates

--- COMP9444/Ass/Ass1/kuzu.py
+++ COMP9444/Ass/Ass1/kuzu.py
@@ -5,20 +5,21 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+import sys
+import numpy
 #！！！！！！！！！！！！Not Completed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 class NetLin(nn.Module):
     # linear function followed by log_softmax
     def __init__(self):
         super(NetLin, self).__init__()
-        self.linear = torch.nn.Linear(28,1)
+        self.linear = torch.nn.Linear(28*28,10)
 
     def forward(self, x):
-        print(x.size())
+        #torch.set_printoptions(profile="full")
+        x = x.view(-1, 28*28)
         out = self.linear(x) 
-        print(out.size())
-        return F.log_softmax(out, dim = 1)
+        return F.log_softmax(out, dim = 0)
 
 class NetFull(nn.Module):
     # two fully connected tanh layers followed by log softmax
