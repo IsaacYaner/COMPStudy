eed54a4b450b1882a2d5ea9d16675c1e224d009c
IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
2022-10-12 00:19:46 +1100

    Update to word parsing

--- COMP4418/ASS/PRO/B/CODE/preprocessing.py
+++ COMP4418/ASS/PRO/B/CODE/preprocessing.py
@@ -1,15 +1,21 @@
 import json
 import pprint
+from windoz.windoz import Stopwatch
 word = 'learning'
 
+
 result_list = []
+timer = Stopwatch()
+
 with open('dblp-ref-0.json') as file:
-    for i in range(1000):
+    while True:
         data = file.readline()
+        if data == '':
+            break
         if word in data:
             obj = json.loads(data)
             result = {}
-            print(obj.keys())
+            # print(obj.keys())
             result['id'] = obj['id']
             try:
                 result['ref'] = obj['references']
@@ -17,4 +23,5 @@ with open('dblp-ref-0.json') as file:
                 result['ref'] = []
             result_list.append(result)
 
-pprint.pprint(result_list)
+print(timer.read_and_set())
+pprint.pprint(len(result_list))
---
+++ COMP6714/PRA/README.md
@@ -0,0 +1,3 @@
+# COMP6714 (Information Retrieval and Web Search) @ UNSW
+
+Course web site: (placeholder)
