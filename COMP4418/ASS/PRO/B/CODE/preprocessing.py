d19422588e17a682d2dd9fe871039ac0775bc3eb
IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
2022-10-17 14:37:19 +1100

    Additive

--- COMP4418/ASS/PRO/B/CODE/preprocessing.py
+++ COMP4418/ASS/PRO/B/CODE/preprocessing.py
@@ -4,28 +4,36 @@ from prefunctions import *
 from windoz.windoz import Stopwatch
 word = 'learning'
 
-
+ith = 0
+rounds = 0
 result_list = []
 timer = Stopwatch()
+files = ['dblp-ref-0.json','dblp-ref-1.json','dblp-ref-2.json','dblp-ref-3.json']
 with open('preprocessed.txt', 'w') as output:
-    with open('dblp-ref-0.json') as file:
-        while True:
-            data = file.readline()
-            if data == '':
-                break
-            result = preprocess(data, True)
-            output.write(result)
-            output.write('\n')
-            # if word in data:
-                # obj = json.loads(data)
-                # result = {}
-                # # print(obj.keys())  
-                # result['id'] = obj['id']
-                # try:
-                #     result['ref'] = obj['references']
-                # except:
-                #     result['ref'] = []
-                # result_list.append(result)
+    for f in files:
+        with open(f) as file:
+            while True:
+                ith += 1
+                data = file.readline()
+                if data == '':
+                    break
+                result = preprocess(data, True)
+                output.write(result)
+                output.write('\n')
+                if ith > 100000:
+                    ith = 0
+                    rounds += 1
+                    print(rounds)
+                # if word in data:
+                    # obj = json.loads(data)
+                    # result = {}
+                    # # print(obj.keys())  
+                    # result['id'] = obj['id']
+                    # try:
+                    #     result['ref'] = obj['references']
+                    # except:
+                    #     result['ref'] = []
+                    # result_list.append(result)
 
 # print(timer.read_and_set())
 # pprint.pprint(len(result_list))
