49dad6f65dc417006c9c1458ffbedd85d64af6ab
IsaacYaner <47761372+IsaacYaner@users.noreply.github.com>
2022-10-19 18:51:33 +1100

    Preprocess finished

--- COMP6714/ASS/prefunctions.py
+++ COMP6714/ASS/prefunctions.py
@@ -2,6 +2,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 import re
+import regex
 stopwords = stopwords.words('english')
 
 '''
@@ -38,6 +39,32 @@ def preprocess(text, link=False, to_lemmatize=False, fix_quotation=True, remove_
     if link:
         return " ".join(text)
     return text
+
+def clean_text(rgx_list, text, flags=[regex.IGNORECASE for dom in range(10)]):
+    new_text = text
+    i = 0
+    for rgx_match in rgx_list:
+        new_text = regex.sub(rgx_match, '', new_text, flags=flags[i])
+        i += 1
+    return new_text
+
+def remove_punctuations(text):
+    ignore_list = [r"(?<!(can|let|it|he|she|there|here|that|this))'(?=[s .])", r"(?<=\.[a-z])\.(?=[ ])", r"(?<=[a-z])\.(?=[a-z])", r"(?<=([A-Z][rs]*|[eE][tT][cC]))\.(?=[A-Z ,.\n])"]
+    flags = [regex.IGNORECASE,regex.IGNORECASE,regex.IGNORECASE, 0]
+    # data = regex.findall(PATTERN, my_string)
+    return clean_text(ignore_list, text, flags)
+
+def sb_pre(text, link=False):
+    text = remove_punctuations(text)
+    text = regex.sub(r'[^\w!?. ]', ' ',text)
+    text = word_tokenize(text)
+    text = normalise(text)
+    text = stem(text)
+    if link:
+        return " ".join(text)
+    return text
+
+
 # text = "Hello, my name is laoyanging, reliable, reliability, dogs!"
 # text = preprocess(text)
 # print(text)
--- COMP6714/ASS/test.py
+++ COMP6714/ASS/test.py
@@ -1,22 +1,15 @@
-from re import IGNORECASE
-import regex
-from nltk.tokenize import RegexpTokenizer
+from prefunctions import sb_pre
 
-def clean_text(rgx_list, text, flags=[regex.IGNORECASE for dom in range(10)]):
-    new_text = text
-    i = 0
-    for rgx_match in rgx_list:
-        new_text = regex.sub(rgx_match, '', new_text, flags=flags[i])
-        i += 1
-    return new_text
 # tokenizer = RegexpTokenizer(r'\w+', gaps=True)
 # data = tokenizer.tokenize('EEighty-seven miles to go, yet.  Onward!')
 # print(" ".join(data))
 my_string = 'Cat\'s shoe\'s cats\' tom\'s hoBys\'.\n\
 I\'m Can\'t Let\'s It\'s He\'s She\'s there\'s here\'s\n\
-Hello, I\'m a U.S. doctor Mr. Marie.\n'
-ignore_list = [r"(?<!(can|let|it|he|she|there|here|that|this))'(?=[s .])", "(?<=[A-Z])\.(?=[A-Z ,.\n])"]
-flags = [regex.IGNORECASE, 0]
-# data = regex.findall(PATTERN, my_string)
-data = clean_text(ignore_list, my_string, flags)
+Hello, I\'m a U.S. doctor Mr. Marie.\n\
+Mrs. Mr. C.N.N. U.S.A. U.K. etc. ETC. Etc. e.g. a.m. p.m. i.e. e.g. p.p. \n\
+I love pineapple. I love it! Do you know Corns?\
+Door-dash'
+
+
+data = sb_pre(my_string, link=True)
 print(data)
